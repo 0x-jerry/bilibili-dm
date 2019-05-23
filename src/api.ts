5bcf20400910540b76915f7b89efdd3f47123d4e
cwxyz007 <jie844067636@gmail.com>
2019-05-23 12:48:46 +0800

    perf: Refactor api

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,13 @@ import axios from 'axios'
 
 const apiRoot = 'https://api.live.bilibili.com'
 
+axios.defaults.baseURL = apiRoot
+
+async function get(path: string, params?: { [key: string]: any }) {
+  const res = await axios.get(path, { params })
+  return res.data.data
+}
+
 export interface IServerConf {
   host: string
   port: number
@@ -9,10 +16,8 @@ export interface IServerConf {
   wss_port: number
 }
 
-export async function getLiveConf(roomID: number) {
-  const url = apiRoot + '/room/v1/Danmu/getConf?room_id=' + roomID
-  const res = await axios.get(url)
-  const data = res.data.data
+async function getLiveConf(roomID: number) {
+  const data = await get('/room/v1/Danmu/getConf', { room_id: roomID })
 
   return {
     host: data.host,
@@ -22,25 +27,18 @@ export async function getLiveConf(roomID: number) {
   }
 }
 
-export async function getRoomID(chatID: number) {
-  const url = apiRoot + '/room/v1/Room/room_init?id=' + chatID
-  const res = await axios.get(url)
-  const data = res.data.data
-  console.log(data)
+async function getRoomInfo(chatID: number) {
+  const data = await get('/room/v1/Room/get_info', { id: chatID })
 
   return {
-    roomId: +data.room_id,
+    roomID: +data.room_id,
     /**
-     * 0 未开播
-     * 1 开播
+     * 0: 未开播
+     * 1: 开播
      */
-    status: +data.live_status
+    status: +data.live_status,
+    cover: data.user_cover
   }
 }
 
-export async function getRoomInfo(roomID: number) {
-  const url = apiRoot + '/room/v1/Room/get_info?id=' + roomID
-
-  const res = await axios.get(url)
-  return res.data as string
-}
+export { get, getLiveConf, getRoomInfo }
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ export * from './msg_model'
 export * from './dm_client'
 export * from './api'
 
-import { getRoomID, getLiveConf } from './api'
+import { getRoomInfo, getLiveConf } from './api'
 import DMClient from './dm_client'
 
 export async function connectToClient(chatID: number) {
-  const liveInfo = await getRoomID(chatID)
+  const roomInfo = await getRoomInfo(chatID)
 
-  const liveConf = await getLiveConf(liveInfo.roomId)
+  const liveConf = await getLiveConf(roomInfo.roomID)
 
-  console.log(liveConf.host, liveConf.port, liveInfo.status)
-  return new DMClient(liveConf.host, liveConf.port, liveInfo.roomId)
+  console.log(liveConf.host, liveConf.port, roomInfo.status)
+  return new DMClient(liveConf.host, liveConf.port, roomInfo.roomID)
 }
